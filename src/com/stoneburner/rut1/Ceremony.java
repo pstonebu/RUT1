ee16321441ec6d02375239c3a9c6f518f6eb511f
Patrick Stoneburner <patrick.stoneburner@jivesoftware.com>
2019-08-23 10:02:38 -0600

    clean up

--- src/com/stoneburner/rut1/Ceremony.java
+++ src/com/stoneburner/rut1/Ceremony.java
@@ -5,13 +5,25 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static java.util.stream.IntStream.iterate;
+import static java.util.stream.IntStream.range;
 
 @Getter @Setter @NoArgsConstructor
 class Ceremony {
     private int numBeams;
     private List<Couple> couples = newArrayList();
+
+    public Ceremony(int numBeams, Person ... people) {
+        this.numBeams = numBeams;
+        iterate(0, i -> i + 2).limit(people.length/2).forEach(i -> {
+            try {
+                couples.add(new Couple(people[i], people[i+1]));
+            } catch (SamePersonException e) { }
+        });
+    }
 
     void addCouple(Person one, Person two) throws SamePersonException {
         couples.add(new Couple(one, two));
--- src/com/stoneburner/rut1/Main.java
+++ src/com/stoneburner/rut1/Main.java
@@ -37,78 +37,23 @@ public class Main {
                 })
                 .collect(toList());
 
-        Ceremony one = new Ceremony();
-        one.addCouple(AASHA, PAIGE);
-        one.addCouple(AMBER, NOUR);
-        one.addCouple(BASIT, JONATHAN);
-        one.addCouple(BRANDON, REMY);
-        one.addCouple(DANNY, KAI);
-        one.addCouple(JASMINE, JENNA);
-        one.addCouple(JUSTIN, MAX);
-        one.addCouple(KARI, KYLIE);
-        one.setNumBeams(2);
-
-        Ceremony two = new Ceremony();
-        two.addCouple(AASHA, BRANDON);
-        two.addCouple(AMBER, NOUR);
-        two.addCouple(BASIT, JONATHAN);
-        two.addCouple(DANNY, REMY);
-        two.addCouple(JASMINE, JUSTIN);
-        two.addCouple(JENNA, KAI);
-        two.addCouple(KARI, KYLIE);
-        two.addCouple(MAX, PAIGE);
-        two.setNumBeams(2);
-
-        Ceremony three = new Ceremony();
-        three.addCouple(AASHA, MAX);
-        three.addCouple(AMBER, PAIGE);
-        three.addCouple(BASIT, REMY);
-        three.addCouple(BRANDON, JONATHAN);
-        three.addCouple(DANNY, KAI);
-        three.addCouple(JASMINE, NOUR);
-        three.addCouple(JENNA, JUSTIN);
-        three.addCouple(KARI, KYLIE);
-        three.setNumBeams(2);
-
-        Ceremony four = new Ceremony();
-        four.addCouple(AASHA, REMY);
-        four.addCouple(AMBER, NOUR);
-        four.addCouple(BASIT, DANNY);
-        four.addCouple(BRANDON, JASMINE);
-        four.addCouple(JENNA, PAIGE);
-        four.addCouple(JONATHAN, KYLIE);
-        four.addCouple(JUSTIN, MAX);
-        four.addCouple(KAI, KARI);
-        four.setNumBeams(1);
-
-        Ceremony six = new Ceremony();
-        six.addCouple(AASHA, BRANDON);
-        six.addCouple(AMBER, JENNA);
-        six.addCouple(BASIT,JONATHAN);
-        six.addCouple(DANNY, KAI);
-        six.addCouple(JASMINE, KYLIE);
-        six.addCouple(JUSTIN, MAX);
-        six.addCouple(KARI, PAIGE);
-        six.addCouple(NOUR, REMY);
-        six.setNumBeams(3);
-
-        Ceremony seven = new Ceremony();
-        seven.addCouple(AASHA, BRANDON);
-        seven.addCouple(AMBER, DANNY);
-        seven.addCouple(BASIT,JONATHAN);
-        seven.addCouple(JASMINE, KAI);
-        seven.addCouple(JENNA, PAIGE);
-        seven.addCouple(JUSTIN, MAX);
-        seven.addCouple(KARI, REMY);
-        seven.addCouple(KYLIE, NOUR);
-        seven.setNumBeams(3);
+        Ceremony one = new Ceremony(2, AASHA, PAIGE, AMBER, NOUR, BASIT, JONATHAN, BRANDON, REMY, DANNY, KAI, JASMINE, JENNA, JUSTIN, MAX, KARI, KYLIE);
+        Ceremony two = new Ceremony(2, AASHA, BRANDON, AMBER, NOUR, BASIT, JONATHAN, DANNY, REMY, JASMINE, JUSTIN, JENNA, KAI, KARI, KYLIE, MAX, PAIGE);
+        Ceremony three = new Ceremony(2, AASHA, MAX, AMBER, PAIGE, BASIT, REMY, BRANDON, JONATHAN, DANNY, KAI, JASMINE, NOUR, JENNA, JUSTIN, KARI, KYLIE);
+        Ceremony four = new Ceremony(1, AASHA, REMY, AMBER, NOUR, BASIT, DANNY, BRANDON, JASMINE, JENNA, PAIGE, JONATHAN, KYLIE, JUSTIN, MAX, KAI, KARI);
+        Ceremony five = new Ceremony(0, AASHA, KAI, AMBER, NOUR, BASIT, REMY, BRANDON, MAX, DANNY, KARI, JASMINE, PAIGE, JENNA, KYLIE, JONATHAN, JUSTIN);
+        Ceremony six = new Ceremony(3, AASHA, BRANDON, AMBER, JENNA, BASIT, JONATHAN, DANNY, KAI, JASMINE, KYLIE, JUSTIN, MAX, KARI, PAIGE, NOUR, REMY);
+        Ceremony seven = new Ceremony(3, AASHA, BRANDON, AMBER, DANNY, BASIT, JONATHAN, JASMINE, KAI, JENNA, PAIGE, JUSTIN, MAX, KARI, REMY, KYLIE, NOUR);
+        //Ceremony eight = new Ceremony(?, AASHA, BRANDON, AMBER, PAIGE, BASIT, JONATHAN, DANNY, KYLIE, JASMINE, NOUR, JENNA, KARI, JUSTIN, MAX, KAI, REMY);
 
         ceremonies.add(one);
         ceremonies.add(two);
         ceremonies.add(three);
         ceremonies.add(four);
+        ceremonies.add(five);
         ceremonies.add(six);
         ceremonies.add(seven);
+        //ceremonies.add(eight);
 
         //No match truth booths
         noMatches.add(new Couple(JUSTIN, NOUR));
@@ -116,15 +61,8 @@ public class Main {
         noMatches.add(new Couple(JENNA, KAI));
         noMatches.add(new Couple(JENNA, DANNY));
         noMatches.add(new Couple(KYLIE, KARI));
-        //adding the blackout from ceremony five here to speed things up
-        noMatches.add(new Couple(AASHA, KAI));
-        noMatches.add(new Couple(AMBER, NOUR));
-        noMatches.add(new Couple(BASIT, REMY));
-        noMatches.add(new Couple(BRANDON, MAX));
-        noMatches.add(new Couple(DANNY, KARI));
-        noMatches.add(new Couple(JASMINE, PAIGE));
-        noMatches.add(new Couple(JENNA, KYLIE));
-        noMatches.add(new Couple(JONATHAN, JUSTIN));
+        noMatches.add(new Couple(JENNA, JASMINE));
+        noMatches.add(new Couple(PAIGE, REMY));
 
         //Perfect match truth booths
         perfectMatches.add(new Couple(AASHA, BRANDON));
